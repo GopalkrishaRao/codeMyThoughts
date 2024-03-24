e5ab70602d2e9bafc851b228cc9f4779aa41056d
GopalkrishaRao <hrgkrao@gmail.com>
2024-03-24 19:38:09 +0530

    hamburger menu

--- assignment1/script.js
+++ assignment1/script.js
@@ -1,9 +1,24 @@
-function myFunction() {
-    var x = document.getElementById("myNavBar");
-    if (x.className === "navBar") {
-      x.className += " responsive";
+let navBar=document.getElementById("myNavBar")
+let hamburgerMenu= document.getElementsByClassName("hamburger")[0];
+let navItems = document.querySelectorAll(".navItem");
+
+// Hamburger Menu  open and close
+function hamburgerSwitch() {
+    if (navBar.className === "navBar") {
+      navBar.className += " responsive";
+      hamburgerMenu.innerHTML="X"
     } else {
-      x.className = "navBar";
+      navBar.className = "navBar";
+      hamburgerMenu.innerHTML=" <i class='fa fa-bars'></i>"
     }
   }
-  
+
+// Close hamburger menu when cliked on nav items
+  navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+        if (navBar.classList.contains("responsive")) {
+            navBar.classList.remove("responsive");
+            hamburgerMenu.innerHTML = " <i class='fa fa-bars'></i>";
+        }
+    });
+});
